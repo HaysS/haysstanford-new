980b7121882a3bda59c704051a38a294f9f3342e
HaysS <stanfordhays@gmail.com>
2018-10-16 22:29:20 -0500

    removed all mainnav links except for the blog page

--- src/components/MainNavBar/MainNavBar.jsx
+++ src/components/MainNavBar/MainNavBar.jsx
@@ -14,15 +14,9 @@ class MainNavBar extends Component {
             <h3 style={{flex: '1', color: '#f7f7f7'}}>Hays Stanford</h3>
           </Link>
           <div style={{flex: '2', color: '#f7f7f7'}}></div>
-          <div style={{flex: '2', display: 'flex', margin: 'auto'}}>
+          <div style={{flex: '1', display: 'flex', margin: 'auto'}}>
             <Link to='/blog/' style={{flex: '1'}}>
               <h5 style={{color: '#f7f7f7'}}>Blog</h5>
-            </Link>
-            <Link to='/about/' style={{flex: '1'}}>
-              <h5 style={{color: '#f7f7f7'}}>About</h5>
-            </Link>
-            <Link to='/contact/' style={{flex: '1'}}>
-              <h5 style={{color: '#f7f7f7'}}>Contact</h5>
             </Link>
           </div>
         </div>
